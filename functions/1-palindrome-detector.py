fa00feda3751c89b067f2283f82c4afea87f3d6b
Adeleke Ogunsona <110996523+adelekeogunsona@users.noreply.github.com>
2023-03-18 14:40:40 +0100

    Improved code readability

--- functions/1-palindrome-detector.py
+++ functions/1-palindrome-detector.py
@@ -14,17 +14,20 @@ def sanitizer(my_string: str) -> str:
 
 # check for palindrome
 def is_palindrome(my_string: str) -> str:
-    if len(my_string) < 2:
-      return False
-    else:
+    if len(my_string) > 1:
       clean_string = sanitizer(my_string)
       reverse = clean_string[::-1]
-      for i in range(len(reverse)):
-        if reverse[i] != clean_string[i]:
-          return False
-    return True
+      if clean_string == reverse:
+        return True
+    return False
 
 
+# test cases
 assert is_palindrome("racecar") == True
 assert is_palindrome("Hello") == False
 assert is_palindrome("A man, a plan, a canal, Panama!") == True
+assert is_palindrome("") == False
+assert is_palindrome("a") == False
+assert is_palindrome("ab") == False
+assert is_palindrome("aa") == True
+assert is_palindrome("A Santa at NASA") == True
