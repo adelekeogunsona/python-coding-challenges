80540cdebaaf51827a1a26ff503f17ad4c0cea52
Adeleke Ogunsona <110996523+adelekeogunsona@users.noreply.github.com>
2023-03-19 14:41:44 +0100

    Added function to check two sum

--- data-structures-and-algorithms/1-two-sum.py
+++ data-structures-and-algorithms/1-two-sum.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+from array import *
+
+def two_sum(input_array: array, target: int) -> array:
+    output_array = array('i')
+    
+    for index1, number1 in enumerate(input_array):
+        for index2 in range(1, len(input_array)):
+            if number1 + input_array[index2] == target:
+                output_array.append(index1)
+                output_array.append(index2)
+                return output_array
+    return output_array
+
+
+nums = [2, 7, 11, 15]
+target = 9
+assert two_sum(nums, target) == array('i', [0, 1])
+
+nums = [3, 2, 4]
+target = 6
+assert two_sum(nums, target) == array('i', [1, 2])
+
+nums = [0, 4, 3, 0]
+target = 0
+assert two_sum(nums, target) == array('i', [0, 3])
+
+nums = []
+target = 8
+assert two_sum(nums, target) == array('i')
--- data-structures-and-algorithms/README.md
+++ data-structures-and-algorithms/README.md
@@ -6,27 +6,33 @@ Given an array of integers, return the indices of the two numbers that add up to
 
 Function signature:
 ```
-def two_sum(nums: List[int], target: int) -> List[int]:
+def two_sum(nums: Array[int], target: int) -> Array[int]:
 ```
 
 Test case 1:
 ```
 nums = [2, 7, 11, 15]
 target = 9
-assert two_sum(nums, target) == [0, 1]
+assert two_sum(nums, target) == array('i', [0, 1])
 ```
 
 Test case 2:
 ```
 nums = [3, 2, 4]
 target = 6
-assert two_sum(nums, target) == [1, 2]
+assert two_sum(nums, target) == array('i', [1, 2])
 ```
 
 Test case 3:
 ```
 nums = [0, 4, 3, 0]
 target = 0
-assert two_sum(nums, target) == [0, 3]
+assert two_sum(nums, target) == array('i', [0, 3])
 ```
 
+Test case 4:
+```
+nums = []
+target = 8
+assert two_sum(nums, target) == array('i')
+```
